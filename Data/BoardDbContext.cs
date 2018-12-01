2cf858aec60f4af47fa48059658e2dc19f803ddd
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-12-02 00:34:15 +0900

    Add database-context

---
+++ Data/BoardDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FriendlyBoard.Server.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendlyBoard.Server.Data {
+  public class BoardDbContext : DbContext {
+    private readonly IHostingEnvironment environment;
+
+    public BoardDbContext(
+      DbContextOptions<BoardDbContext> options,
+      IHostingEnvironment environment
+    ) : base(options) {
+      this.environment = environment;
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Thread> Threads { get; set; }
+    public DbSet<Post> Posts { get; set; }
+  }
+}
