8c662645b47bd273e9a59b8367752caef33d0907
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-12-01 22:03:03 +0900

    Update Post entity

--- Entities/Post.cs
+++ Entities/Post.cs
@@ -1,5 +1,13 @@
-﻿namespace FriendlyBoard.Server.Entities {
-  public class Post {
+﻿using System;
 
+namespace FriendlyBoard.Server.Entities {
+  public class Post {
+    public int PostId { get; set; }
+    public DateTime PostedAt { get; set; }
+    public User PostedBy { get; set; }
+    public string Comment { get; set; }
+
+    public int BoardId { get; set; }
+    public Board Board { get; set; }
   }
 }
