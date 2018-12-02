08729bda4d1f4cedf01724b40a02a91ce7789d80
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-12-03 00:00:02 +0900

    Add SystemClock

---
+++ SystemClock.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FriendlyBoard.Server {
+  public interface IClock {
+    DateTime Now { get; }
+  }
+
+  public class SystemClock : IClock {
+    public DateTime Now => DateTime.Now;
+  }
+}
